90b7ea2ef1ddc37284b260e0d8f4e0c53ff01e5e
Thomas Roger <troger@nuxeo.com>
2011-12-26 15:20:06 +0100

    NXP-8227: improve RSS Reader configuration popup

--- nuxeo-rss-reader-ui/src/main/java/org/nuxeo/rss/reader/webengine/RssReaderModuleRoot.java
+++ nuxeo-rss-reader-ui/src/main/java/org/nuxeo/rss/reader/webengine/RssReaderModuleRoot.java
@@ -3,7 +3,9 @@ package org.nuxeo.rss.reader.webengine;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -24,13 +26,14 @@ import org.nuxeo.runtime.api.Framework;
 @Produces("text/html; charset=UTF-8")
 public class RssReaderModuleRoot extends ModuleRoot {
 
-    protected RSSFeedService rssFeedService;
-
     @GET
     @Path("/item")
     public Object getItemContent(@QueryParam("i") String id) throws Exception {
-        List<String> urls = getRSSService().getCurrentUserRssFeedAddresses(ctx.getCoreSession());
-        return getView("item").args(FeedHelper.searchFeedEntry(urls.toArray(new String[urls.size()]), id));
+        RSSFeedService rssFeedService = Framework.getLocalService(RSSFeedService.class);
+        List<String> urls = rssFeedService.getCurrentUserRssFeedAddresses(ctx.getCoreSession());
+        return getView("item").args(
+                FeedHelper.searchFeedEntry(
+                        urls.toArray(new String[urls.size()]), id));
     }
 
     @GET
@@ -44,10 +47,9 @@ public class RssReaderModuleRoot extends ModuleRoot {
     public Object getConfigPage() throws Exception {
         CoreSession session = ctx.getCoreSession();
         boolean isAbleToCreateNew = false;
-        DocumentModelList userFeeds = getRSSService().getCurrentUserRssFeedDocumentModelList(
-                session);
-        DocumentModelList globalFeeds = getRSSService().getGlobalFeedsDocumentModelList(
-                session);
+        RSSFeedService rssFeedService = Framework.getLocalService(RSSFeedService.class);
+        DocumentModelList userFeeds = rssFeedService.getCurrentUserRssFeedDocumentModelList(session);
+        DocumentModelList globalFeeds = rssFeedService.getGlobalFeedsDocumentModelList(session);
         List<DocumentModel> options = new ArrayList<DocumentModel>();
         if (userFeeds != null && globalFeeds != null) {
             for (DocumentModel f1 : globalFeeds) {
@@ -64,39 +66,52 @@ public class RssReaderModuleRoot extends ModuleRoot {
                     options.add(f1);
                 }
             }
-            isAbleToCreateNew = userFeeds.size() < getRSSService().getMaximumFeedsCount(session);
+            isAbleToCreateNew = userFeeds.size() < rssFeedService.getMaximumFeedsCount(session);
         }
 
         return getView("feed_configuration").arg("userFeeds", userFeeds).arg(
-                "globalFeeds", options).arg("ableToCreateNew", isAbleToCreateNew);
+                "globalFeeds", options).arg("ableToCreateNew",
+                isAbleToCreateNew).arg("maxFeedsCount", rssFeedService.getMaximumFeedsCount(session));
     }
 
-    @GET
+    @POST
     @Path("/removeFeed")
-    public Object removeFeed(@QueryParam("id") String id) throws Exception {
+    public Object removeFeed(@FormParam("id") String id) throws Exception {
         CoreSession session = ctx.getCoreSession();
         session.removeDocument(new IdRef(id));
         session.save();
         return getConfigPage();
     }
 
-    @GET
+    @POST
     @Path("/addGlobalFeed")
-    public Object addGlobalFeed(@QueryParam("feedId") String feedId)
+    public Object addGlobalFeed(@FormParam("feedId") String feedId)
             throws Exception {
         CoreSession session = ctx.getCoreSession();
-        String userFeedsContainer = getRSSService().getCurrentUserRssFeedsContainer(
+        RSSFeedService rssFeedService = Framework.getLocalService(RSSFeedService.class);
+        DocumentModelList userFeeds = rssFeedService.getCurrentUserRssFeedDocumentModelList(session);
+        if (userFeeds != null && userFeeds.size() >= rssFeedService.getMaximumFeedsCount(session)) {
+            return getConfigPage();
+        }
+
+        String userFeedsContainer = rssFeedService.getCurrentUserRssFeedsContainer(
                 session).getPathAsString();
         session.copy(new IdRef(feedId), new PathRef(userFeedsContainer), null);
         return getConfigPage();
     }
 
-    @GET
+    @POST
     @Path("/addNewFeed")
-    public Object addGlobalFeed(@QueryParam("feedName") String feedName,
-            @QueryParam("feedLink") String url) throws Exception {
+    public Object addGlobalFeed(@FormParam("feedName") String feedName,
+            @FormParam("feedLink") String url) throws Exception {
         CoreSession session = ctx.getCoreSession();
-        String userFeedsContainerPath = getRSSService().getCurrentUserRssFeedsContainer(
+        RSSFeedService rssFeedService = Framework.getLocalService(RSSFeedService.class);
+        DocumentModelList userFeeds = rssFeedService.getCurrentUserRssFeedDocumentModelList(session);
+        if (userFeeds != null && userFeeds.size() >= rssFeedService.getMaximumFeedsCount(session)) {
+            return getConfigPage();
+        }
+
+        String userFeedsContainerPath = rssFeedService.getCurrentUserRssFeedsContainer(
                 session).getPathAsString();
         DocumentModel feed = session.createDocumentModel(
                 userFeedsContainerPath, feedName, "RssFeed");
@@ -105,17 +120,6 @@ public class RssReaderModuleRoot extends ModuleRoot {
         feed = session.createDocument(feed);
         session.save();
         return getConfigPage();
-    }
-
-    /**
-     * @return
-     * @throws Exception
-     */
-    protected RSSFeedService getRSSService() throws Exception {
-        if (rssFeedService == null) {
-            rssFeedService = Framework.getService(RSSFeedService.class);
-        }
-        return rssFeedService;
     }
 
 }
--- nuxeo-rss-reader-ui/src/main/resources/skin/views/rssreader/feed_configuration.ftl
+++ nuxeo-rss-reader-ui/src/main/resources/skin/views/rssreader/feed_configuration.ftl
@@ -2,66 +2,89 @@
         "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="${skinPath}/css/feed_configuration.css"/>
   <title>Nuxeo RSS Reader Configuration</title>
+
+  <link rel="stylesheet" type="text/css" href="${skinPath}/css/feed_configuration.css"/>
+  <script type="text/javascript" src="${skinPath}/script/jquery/jquery.js"></script>
+
   <script type="text/javascript">
-    function submitForm(formName) {
+    function submitForm() {
+      var selectedOption = $("#feedId option:selected");
+      var formName = "addGlobalFeedForm";
+      if (selectedOption.length == 0 || selectedOption.val() === "none") {
+        formName = "addNewFeedForm";
+      }
       document.forms[formName].submit();
     }
+
     function closePopUp() {
       parent.jQuery.fancybox.close();
     }
+
+    $(document).ready(function() {
+      $(".field").focus(function() {
+        $("#feedId option:first").attr("selected", true);
+      });
+      $(".field").click(function() {
+    $(this).focus();
+    $(this).select();
+      });
+    });
   </script>
 </head>
 <body>
 
   <div class="feedBlock">
-    Current Feeds:
-    <ul>
+    <h3>${Context.getMessage('label.rss.feed.configuration.title')}</h3>
 
-    <#list userFeeds as feed>
-      <li>
-        <a href="removeFeed?id=${feed.id}" class="removeFeed"><img
-                src="${skinPath}/icons/remove.png" alt=""/></a>
-      ${feed["dc:title"]}- <span>${feed["rf:rss_address"]}</span>
-      </li>
-    </#list>
-    </ul>
+    <h4>${Context.getMessage('label.rss.feed.configuration.add.feed.title')}</h4>
 
-    <#if ableToCreateNew>
-      <#if globalFeeds?has_content>
-        <form id="addGlobaFeedForm" method="GET" action="addGlobalFeed">
-          Add a defined feed:
-          <div class="fieldRow">
-            <select name="feedId" size="0">
-              <#list globalFeeds as feed>
-                <option value="${feed.id}">${feed["dc:title"]}</option>
-              </#list>
-            </select>
-            <a href="javascript: submitForm('addGlobaFeedForm')"
-               class="addGlobalFeed"><img src="${skinPath}/icons/add.png"
-                                          alt=""/></a>
-          </div>
-        </form>
-      </#if>
-
-      Add a new feed:
-      <form id="addNewFeedForm" method="GET" action="addNewFeed">
-        <div class="fieldRow">
-          <input class="field" type="text" name="feedName" value="Feed Title"/>
-          <input class="field feedlink" type="text" name="feedLink"
-                 value="Feed link"/>
-          <a href="javascript: submitForm('addNewFeedForm')" class="addNewFeed">
-            <img src="${skinPath}/icons/add.png" alt=""/>
-          </a>
-        </div>
+    <#if globalFeeds?has_content>
+      <form id="addGlobalFeedForm" method="POST" action="addGlobalFeed">
+        <select id="feedId" name="feedId" size="0">
+          <option value="none">${Context.getMessage('label.rss.feed.configuration.select.defined.feed')}</option>
+          <#list globalFeeds as feed>
+            <option value="${feed.id}">${feed["dc:title"]}</option>
+          </#list>
+        </select>
       </form>
     </#if>
-  </div>
 
-  <div class="submitBar">
-    <input type="button" value="Done" onclick="javascript:closePopUp()" />
-  </div>
+    ${Context.getMessage('label.rss.feed.configuration.add.custom.feed')}
+
+    <form id="addNewFeedForm" method="POST" action="addNewFeed">
+      <input class="field" type="text" name="feedName"
+        value="${Context.getMessage('label.rss.feed.configuration.custom.feed.title')}"/>
+      <input class="field feedlink" type="text" name="feedLink"
+        value="${Context.getMessage('label.rss.feed.configuration.custom.feed.link')}"/>
+    </form>
+
+    <button <#if ableToCreateNew>onclick="submitForm()"<#else>disabled="disabled" </#if> >${Context.getMessage('label.rss.feed.configuration.feed.add')}</button>
+
+    <h4>${Context.getMessage('label.rss.feed.configuration.receiving.feeds.title1')} ${userFeeds?size} ${Context.getMessage('label.rss.feed.configuration.receiving.feeds.title2')}</h4>
+
+    <p>${Context.getMessage('label.rss.feed.configuration.feeds.limit1')} ${maxFeedsCount} ${Context.getMessage('label.rss.feed.configuration.feeds.limit2')}</p>
+
+    <table>
+      <tbody>
+      <#list userFeeds as feed>
+      <tr>
+        <td>${feed["dc:title"]}</td>
+        <td>${feed["rf:rss_address"]}</td>
+        <td>
+          <form method="POST" action="removeFeed">
+            <input type="hidden" name="id" value="${feed.id}" />
+            <input type="submit" value="${Context.getMessage('label.rss.feed.configuration.feed.remove')}"/>
+          </form>
+        </td>
+      </tr>
+      </#list>
+      </tbody>
+    </table>
+
+    <div class="actionsBar">
+      <button onclick="javascript:closePopUp()">${Context.getMessage('label.rss.feed.configuration.close')}</button>
+    </div>
 
 </body>
 </html>
